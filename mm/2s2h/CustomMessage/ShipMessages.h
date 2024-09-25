f8a2018749c0e2a7cbb584b72499017d7ed08fa7
Garrett Cox <garrettjcox@gmail.com>
2024-09-24 23:09:33 -0500

    Tweaks to give item messaging

--- mm/2s2h/CustomMessage/ShipMessages.h
+++ mm/2s2h/CustomMessage/ShipMessages.h
@@ -6,8 +6,8 @@ DEFINE_MESSAGE(HELLO_WORLD, 0x00, "\x06\x00" "\xFE" "\xFF\xFF" "\xFF\xFF" "\xFF\
 "                   - The Moon"
 )
 DEFINE_MESSAGE(GIVE_ITEM, 0x00, "\x02\x00" "\x0C" "\xFF\xFF" "\xFF\xFF" "\xFF\xFF" "\xFF\xFF"
-"You received a(n) {{item}}!"
+"You received {{item}}!"
 )
 DEFINE_MESSAGE(GIVE_ITEM_NO_STOP, 0x00, "\x02\x00" "\x0C" "\xFF\xFF" "\xFF\xFF" "\xFF\xFF" "\xFF\xFF"
-"You received a(n) {{item}}!" "\x1C\x02\x10"
+"You received {{item}}!" "\x1C\x02\x10"
 )
