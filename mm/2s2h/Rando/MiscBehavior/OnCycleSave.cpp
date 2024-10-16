a06d8be911a47306bac3632de7c261ee5d0a28f1
Garrett Cox <garrettjcox@gmail.com>
2024-10-16 15:39:12 -0500

    Naively persist a bunch of flags through cycle reset, probably will need more iteration

--- mm/2s2h/Rando/MiscBehavior/OnCycleSave.cpp
+++ mm/2s2h/Rando/MiscBehavior/OnCycleSave.cpp
@@ -5,12 +5,28 @@ extern "C" {
 #include <variables.h>
 }
 
-SaveInfo saveInfoCopyForRando;
+SaveContext saveContextCopy;
 
 void Rando::MiscBehavior::BeforeEndOfCycleSave() {
-    memcpy(&saveInfoCopyForRando, &gSaveContext.save.saveInfo, sizeof(SaveInfo));
+    memcpy(&saveContextCopy, &gSaveContext, sizeof(SaveContext));
 }
 
 void Rando::MiscBehavior::AfterEndOfCycleSave() {
-    // TODO: Tbh idk what exactly to persist, come back here later
+    for (int i = 0; i < 8; i++) {
+        gSaveContext.save.saveInfo.inventory.dungeonItems[i] = saveContextCopy.save.saveInfo.inventory.dungeonItems[i];
+        gSaveContext.save.saveInfo.inventory.dungeonKeys[i] = saveContextCopy.save.saveInfo.inventory.dungeonKeys[i];
+        gSaveContext.save.saveInfo.inventory.strayFairies[i] = saveContextCopy.save.saveInfo.inventory.strayFairies[i];
+    }
+
+    // Naively persist all cycle scene flags, we'll likely need to be more selective.
+    for (int i = 0; i < SCENE_MAX; i++) {
+        gSaveContext.cycleSceneFlags[i].chest = saveContextCopy.cycleSceneFlags[i].chest;
+        gSaveContext.cycleSceneFlags[i].collectible = saveContextCopy.cycleSceneFlags[i].collectible;
+        gSaveContext.cycleSceneFlags[i].switch0 = saveContextCopy.cycleSceneFlags[i].switch0;
+        gSaveContext.cycleSceneFlags[i].switch1 = saveContextCopy.cycleSceneFlags[i].switch1;
+    }
+
+    if (RANDO_SAVE_OPTIONS[RO_SHUFFLE_GOLD_SKULLTULAS]) {
+        gSaveContext.save.saveInfo.skullTokenCount = saveContextCopy.save.saveInfo.skullTokenCount;
+    }
 }
