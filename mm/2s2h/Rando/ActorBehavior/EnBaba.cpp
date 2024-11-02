17f93a270e2be66933cc8539bd246533aa8f0827
Eblo <7004497+Eblo@users.noreply.github.com>
2024-11-01 21:01:57 -0400

    Fix bug with Bomb Shop Lady check handling (#27)

--- mm/2s2h/Rando/ActorBehavior/EnBaba.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnBaba.cpp
@@ -14,18 +14,21 @@ void EnBaba_GaveBlastMask(EnBaba* enBaba, PlayState* play);
 // this case, as Link can move freely once the next textbox appears. This code fixes that.
 void Rando::ActorBehavior::InitEnBabaBehavior() {
     COND_VB_SHOULD(VB_GIVE_ITEM_FROM_OFFER, IS_RANDO, {
-        *should = false;
-
         GetItemId* item = va_arg(args, GetItemId*);
-        EnBaba* enBaba = va_arg(args, EnBaba*);
-        Player* player = GET_PLAYER(gPlayState);
-        enBaba->stateFlags |= BOMB_SHOP_LADY_STATE_GAVE_BLAST_MASK;
-        enBaba->actionFunc = EnBaba_GaveBlastMask;
-        enBaba->actor.parent = &player->actor;
-        player->talkActor = &enBaba->actor;
-        player->talkActorDistance = enBaba->actor.xzDistToPlayer;
-        player->exchangeItemAction = PLAYER_IA_MINUS1;
-        Player_TalkWithPlayer(gPlayState, &enBaba->actor);
-        RANDO_SAVE_CHECKS[RC_CLOCK_TOWN_NORTH_BOMB_LADY].eligible = true;
+        Actor* actor = va_arg(args, Actor*);
+        // This runs for all actors using Actor_OfferGetItem, so make sure we only do this with the Bomb Shop Lady.
+        if (actor->id == ACTOR_EN_BABA) {
+            *should = false;
+            EnBaba* enBaba = (EnBaba*)actor;
+            Player* player = GET_PLAYER(gPlayState);
+            enBaba->stateFlags |= BOMB_SHOP_LADY_STATE_GAVE_BLAST_MASK;
+            enBaba->actionFunc = EnBaba_GaveBlastMask;
+            enBaba->actor.parent = &player->actor;
+            player->talkActor = &enBaba->actor;
+            player->talkActorDistance = enBaba->actor.xzDistToPlayer;
+            player->exchangeItemAction = PLAYER_IA_MINUS1;
+            Player_TalkWithPlayer(gPlayState, &enBaba->actor);
+            RANDO_SAVE_CHECKS[RC_CLOCK_TOWN_NORTH_BOMB_LADY].eligible = true;
+        }
     });
 }
