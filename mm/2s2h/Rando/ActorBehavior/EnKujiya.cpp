4f2664d9c8935213554f107c89ce6a6d7aff5caf
Caladius <Caladius@users.noreply.github.com>
2024-12-26 23:57:29 -0500

    Update Dialogue to reflect Rando Reward (#91)

--- mm/2s2h/Rando/ActorBehavior/EnKujiya.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnKujiya.cpp
@@ -7,6 +7,17 @@ extern "C" {
 void EnKujiya_Wait(EnKujiya* enKujiya, PlayState* play);
 }
 
+std::string IdentifyItemName() {
+    std::string itemName = Rando::StaticData::Items[RANDO_SAVE_CHECKS[RC_CLOCK_TOWN_WEST_LOTTERY].randoItemId].article;
+    if (itemName != "") {
+        itemName += " ";
+    }
+    itemName += Rando::StaticData::Items[RANDO_SAVE_CHECKS[RC_CLOCK_TOWN_WEST_LOTTERY].randoItemId].name;
+    itemName += '\x00';
+
+    return itemName;
+}
+
 void Rando::ActorBehavior::InitEnKujiyaBehavior() {
     COND_VB_SHOULD(VB_GIVE_LOTTERY_WINNINGS, IS_RANDO, {
         EnKujiya* refActor = va_arg(args, EnKujiya*);
@@ -14,5 +25,28 @@ void Rando::ActorBehavior::InitEnKujiyaBehavior() {
         RANDO_SAVE_CHECKS[RC_CLOCK_TOWN_WEST_LOTTERY].eligible = true;
         refActor->actionFunc = EnKujiya_Wait;
         *should = false;
+    });
+
+    COND_ID_HOOK(OnOpenText, 0x2b5c, IS_RANDO, [](u16* textId, bool* loadFromMessageTable) {
+        auto entry = CustomMessage::LoadVanillaMessageTableEntry(*textId);
+
+        entry.autoFormat = false;
+        CustomMessage::Replace(&entry.msg, "50 Rupees", IdentifyItemName());
+        entry.msg.replace(entry.msg.end() - 38, entry.msg.end() - 3, "");
+
+        CustomMessage::LoadCustomMessageIntoFont(entry);
+        *loadFromMessageTable = false;
+    });
+
+    COND_ID_HOOK(OnOpenText, 0x2b66, IS_RANDO, [](u16* textId, bool* loadFromMessageTable) {
+        auto entry = CustomMessage::LoadVanillaMessageTableEntry(*textId);
+        std::string itemString = "\x11";
+        itemString += IdentifyItemName();
+
+        entry.autoFormat = false;
+        CustomMessage::Replace(&entry.msg, "50 Rupees", itemString);
+
+        CustomMessage::LoadCustomMessageIntoFont(entry);
+        *loadFromMessageTable = false;
     });
 }
