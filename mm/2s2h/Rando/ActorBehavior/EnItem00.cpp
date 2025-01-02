bb6359051a6d73fb652252536ffb01ee8fff5422
Caladius <Caladius@users.noreply.github.com>
2025-01-01 21:12:53 -0500

    Freestanding Shuffle Additions (#116)
    * Freestanding Rupees (1st Pass) South Region
    
    * Freestanding (1st Pass) North Region
    
    * Freestanding (1st Pass) - East Region
    
    * clang
    
    * Freestanding Rupees (1st Pass) Woodfall Temple
    
    * Fix Woodfall Scenes
    
    * Freestanding Rupees (1st Pass) Snowhead Temple
    
    * Freestanding Rupees (1st Pass) Great Bay Temple
    
    * Fix Deku Palace Rupees without flags
    
    * Freestanding Rupees (1st Pass) Stone Tower Temple/Inverted
    
    * undo alignment
    
    * Remove debugging tool for item placement
    
    * fix abc order

--- mm/2s2h/Rando/ActorBehavior/EnItem00.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnItem00.cpp
@@ -7,6 +7,17 @@ extern "C" {
 #include "variables.h"
 }
 
+std::map<std::pair<float, float>, RandoCheckId> freestandingMap = {
+    // Deku Palace (TODO: fix Item Gets, items too close?)
+    { { 439.640961f, 1368.999390f }, RC_DEKU_PALACE_FREESTANDING_RUPEE_12 },
+    { { 439.640961f, 1329.000610f }, RC_DEKU_PALACE_FREESTANDING_RUPEE_13 },
+    { { 405.0f, 1309.0f }, RC_DEKU_PALACE_FREESTANDING_RUPEE_14 },
+    { { 370.359039f, 1329.000610f }, RC_DEKU_PALACE_FREESTANDING_RUPEE_15 },
+    { { 370.359039f, 1368.999390f }, RC_DEKU_PALACE_FREESTANDING_RUPEE_16 },
+    { { 405.0, 1389.0 }, RC_DEKU_PALACE_FREESTANDING_RUPEE_17 },
+    { { 405.0, 1349.0 }, RC_DEKU_PALACE_FREESTANDING_RUPEE_18 },
+};
+
 void Rando::ActorBehavior::InitEnItem00Behavior() {
     COND_ID_HOOK(ShouldActorInit, ACTOR_EN_ITEM00, IS_RANDO, [](Actor* actor, bool* should) {
         EnItem00* item00 = (EnItem00*)actor;
@@ -19,7 +30,12 @@ void Rando::ActorBehavior::InitEnItem00Behavior() {
         auto randoStaticCheck = Rando::StaticData::GetCheckFromFlag(FLAG_CYCL_SCENE_COLLECTIBLE,
                                                                     ENITEM00_GET_7F00(actor), gPlayState->sceneId);
         if (randoStaticCheck.randoCheckId == RC_UNKNOWN) {
-            return;
+            auto it = freestandingMap.find({ item00->actor.home.pos.x, item00->actor.home.pos.z });
+            if (it == freestandingMap.end()) {
+                return;
+            } else {
+                randoStaticCheck = Rando::StaticData::Checks[it->second];
+            }
         }
 
         // Pots handle their own items, ignore them
@@ -36,13 +52,30 @@ void Rando::ActorBehavior::InitEnItem00Behavior() {
         // Prevent the original item from spawning
         *should = false;
 
+        // Secret Shrines Freestanding Rupees spawn too high and are out of collection range using CustomItem,
+        // so we lower it slightly so Human Link can reach them.
+        float posY = actor->world.pos.y;
+        if (randoStaticCheck.randoCheckId >= RC_SECRET_SHRINE_FREESTANDING_RUPEE_01 &&
+            randoStaticCheck.randoCheckId <= RC_SECRET_SHRINE_FREESTANDING_RUPEE_17) {
+            posY -= 20.0f;
+        }
         // If it hasn't been collected yet, spawn a dummy item
         CustomItem::Spawn(
-            actor->world.pos.x, actor->world.pos.y, actor->world.pos.z, 0, CustomItem::KILL_ON_TOUCH,
-            randoStaticCheck.randoCheckId,
+            actor->world.pos.x, posY, actor->world.pos.z, 0, CustomItem::KILL_ON_TOUCH, randoStaticCheck.randoCheckId,
             [](Actor* actor, PlayState* play) {
                 auto& randoStaticCheck = Rando::StaticData::Checks[(RandoCheckId)CUSTOM_ITEM_PARAM];
-                Flags_SetCollectible(play, randoStaticCheck.flag);
+                switch (randoStaticCheck.flagType) {
+                    case FLAG_NONE:
+                        if (RANDO_SAVE_CHECKS[randoStaticCheck.randoCheckId].shuffled) {
+                            RANDO_SAVE_CHECKS[randoStaticCheck.randoCheckId].eligible = true;
+                        }
+                        break;
+                    case FLAG_CYCL_SCENE_COLLECTIBLE:
+                        Flags_SetCollectible(play, randoStaticCheck.flag);
+                        break;
+                    default:
+                        break;
+                }
             },
             [](Actor* actor, PlayState* play) {
                 auto& randoSaveCheck = RANDO_SAVE_CHECKS[CUSTOM_ITEM_PARAM];
