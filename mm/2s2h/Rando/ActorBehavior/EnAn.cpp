a1af32c9535dcfe1906d5a2e6b3c8ee7c317b480
Eblo <7004497+Eblo@users.noreply.github.com>
2025-01-06 11:58:50 -0500

    Fix Anju crash on Letter to Kafei check (#138)

--- mm/2s2h/Rando/ActorBehavior/EnAn.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnAn.cpp
@@ -18,15 +18,18 @@ void Rando::ActorBehavior::InitEnAnBehavior() {
 
             if (cmdId == MSCRIPT_CMD_06) { // MSCRIPT_OFFER_ITEM
                 func_80832558(gPlayState, player, func_80837B60);
-                player->talkActor = actor;
                 *should = false;
                 skipCmds.clear();
                 skipCmds.push_back(MSCRIPT_CMD_12); // Have to skip this to prevent a crash
                 skipCmds.push_back(MSCRIPT_CMD_07); // And have to skip this to prevent a softlock on repeats
                 GetItemId getItemId = (GetItemId)MSCRIPT_GET_16(script, 1);
-                // If these are skipped but not manually queued, the game will crash for some reason. This does not
-                // happen with the room key check.
-                if (getItemId == GI_LETTER_TO_KAFEI) {
+                /*
+                 * If the player has the Bombers' Notebook and this is the Letter to Kafei check, the game will crash
+                 * unless player->talkActor is set AND these skipped notebook events are queued. This does not happen
+                 * with the Room Key check.
+                 */
+                if (CHECK_QUEST_ITEM(QUEST_BOMBERS_NOTEBOOK) && getItemId == GI_LETTER_TO_KAFEI) {
+                    player->talkActor = actor;
                     Message_BombersNotebookQueueEvent(gPlayState, BOMBERS_NOTEBOOK_EVENT_PROMISED_TO_MEET_KAFEI);
                     Message_BombersNotebookQueueEvent(gPlayState, BOMBERS_NOTEBOOK_EVENT_RECEIVED_LETTER_TO_KAFEI);
                 }
