1a6acf1660121d87bc6e27c17dbf03be2ba807ea
Garrett Cox <garrettjcox@gmail.com>
2025-01-28 21:16:17 -0600

    More one point skips

--- mm/2s2h/Enhancements/Cutscenes/SkipOnePointCutscenes.cpp
+++ mm/2s2h/Enhancements/Cutscenes/SkipOnePointCutscenes.cpp
@@ -10,6 +10,18 @@ extern "C" {
 #define CVAR CVarGetInteger(CVAR_NAME, 0)
 
 void RegisterSkipOnePointCutscenes() {
+    COND_VB_SHOULD(VB_CAMERA_SET_FOCAL_ACTOR, CVAR, {
+        Actor* actor = va_arg(args, Actor*);
+
+        switch (actor->id) {
+            case ACTOR_EN_BAL:
+                *should = false;
+                break;
+            default:
+                break;
+        }
+    });
+
     COND_VB_SHOULD(VB_START_CUTSCENE, CVAR, {
         s16* csId = va_arg(args, s16*);
         Actor* actor = va_arg(args, Actor*);
@@ -68,6 +80,19 @@ void RegisterSkipOnePointCutscenes() {
             case ACTOR_OBJ_CHAN:
             case ACTOR_EN_MM:
             case ACTOR_BG_F40_BLOCK:
+            case ACTOR_EN_BAL:
+            case ACTOR_BG_TOBIRA01:
+            case ACTOR_OBJ_BIGICICLE:
+            case ACTOR_OBJ_HAKAISI:
+            case ACTOR_BG_HAKA_BOMBWALL:
+            case ACTOR_EN_DINOFOS:
+            case ACTOR_EN_DRAGON:
+            case ACTOR_BG_DBLUE_BALANCE:
+            case ACTOR_BG_DBLUE_WATERFALL:
+            case ACTOR_OBJ_ICEBLOCK:
+            case ACTOR_BG_IKNIN_SUSCEIL:
+            case ACTOR_BG_IKANA_DHARMA:
+            case ACTOR_OBJ_HUGEBOMBIWA:
                 *should = false;
                 break;
             default:
