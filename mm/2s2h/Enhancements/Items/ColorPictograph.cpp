3a7a9a7d91b827a7397dfa9c39df833ddcc46906
sitton76 <58642183+sitton76@users.noreply.github.com>
2026-03-19 21:17:29 -0500

    Changed to pointer(FIX FOR LINUX)

--- mm/2s2h/Enhancements/Items/ColorPictograph.cpp
+++ mm/2s2h/Enhancements/Items/ColorPictograph.cpp
@@ -161,7 +161,7 @@ void RegisterColorPictograph() {
     });
 
     COND_VB_SHOULD(VB_PICTO_DISPLAY, CVAR, {
-        s16 sp2CC = va_arg(args, s16);
+        s16* sp2CC = va_arg(args, s16*);
 
         // might need something better to check for existance
         if (pictoPhotoRGBABuffer[0] != 0) {
